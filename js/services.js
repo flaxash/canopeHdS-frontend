c794bdc6962ea9a5585fbfa6f4e0b20a0e4dd783
flaxash <xavier@MacBook-Pro-de-xavier.local>
2017-03-06 13:01:58 +0100

    passage en https
    Modification des liens statiques pour prendre en compte le protocole
    https

--- js/services.js
+++ js/services.js
@@ -18,20 +18,20 @@ cddp92App.factory('StateService', function () {
 			 return {
      			getActualites: function(callback) {
        			//$http.get('http://www.cddp92.ac-versailles.fr/drupalCddp/?q=gateway/views/actualitesApercu.json').success(callback);
-				$http.get('http://www.reseau-canope.fr/atelier-hauts-de-seine/drupal7/?q=gateway/views/actualites.json').success(callback);
+				$http.get('https://www.reseau-canope.fr/atelier-hauts-de-seine/drupal7/?q=gateway/views/actualites.json').success(callback);
 				},
 				getConferences:function(callback) {
 				//$http.get('http://www.cddp92.ac-versailles.fr/drupalCddp/?q=gateway/views/conferencesApercu.json').success(callback);
-				$http.get('http://www.reseau-canope.fr/atelier-hauts-de-seine/drupal7/?q=gateway/views/conferences.json').success(callback);	
+				$http.get('https://www.reseau-canope.fr/atelier-hauts-de-seine/drupal7/?q=gateway/views/conferences.json').success(callback);	
 				},
 				getConfsPreview:function(callback) {
-					$http.get('http://www.reseau-canope.fr/atelier-hauts-de-seine/drupal7/?q=gateway/views/confspreview.json').success(callback);	
+					$http.get('https://www.reseau-canope.fr/atelier-hauts-de-seine/drupal7/?q=gateway/views/confspreview.json').success(callback);	
 				},
 				getActusPreview:function(callback) {
-					$http.get('http://www.reseau-canope.fr/atelier-hauts-de-seine/drupal7/?q=gateway/views/actuspreview.json').success(callback);	
+					$http.get('https://www.reseau-canope.fr/atelier-hauts-de-seine/drupal7/?q=gateway/views/actuspreview.json').success(callback);	
 				},
         getRssFromActusNum:function(callback) {
-          $http.get('http://blog.crdp-versailles.fr/usagestice92/index.php/feed/atom').success(callback);
+          $http.get('https://blog.crdp-versailles.fr/usagestice92/index.php/feed/atom').success(callback);
         }
 			 }
 	})
@@ -40,7 +40,7 @@ cddp92App.factory('StateService', function () {
             	getSlides: function(callback) {
        			//$http.get('http://www.cddp92.ac-versailles.fr/aLaUne/listeImages.xml').success(callback);
 				$http.get(
-                    "http://www.reseau-canope.fr/atelier-hauts-de-seine/aLaUne/listeImages.xml?t="+Math.random(),
+                    "https://www.reseau-canope.fr/atelier-hauts-de-seine/aLaUne/listeImages.xml?t="+Math.random(),
                     {transformResponse:function(data) {
                       // convert the data to JSON and provide
                       // it to the success function below
@@ -61,7 +61,7 @@ cddp92App.factory('StateService', function () {
 				getMessage: function(callback) {
        			//$http.get('http://www.cddp92.ac-versailles.fr/aLaUne/listeImages.xml').success(callback);
 				$http.get(
-                    "http://www.reseau-canope.fr/atelier-hauts-de-seine/aLaUne/listeImages.xml?t="+Math.random(),
+                    "https://www.reseau-canope.fr/atelier-hauts-de-seine/aLaUne/listeImages.xml?t="+Math.random(),
                     {transformResponse:function(data) {
                       // convert the data to JSON and provide
                       // it to the success function below
